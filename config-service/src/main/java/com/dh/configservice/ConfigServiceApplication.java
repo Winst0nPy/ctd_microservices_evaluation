6a7545e6adf96101c734ff052047cc0cb10061a7
Daniel Cagliari <dacagliari@gmail.com>
2022-08-29 17:27:44 -0300

    fixing config

--- config-service/src/main/java/com/dh/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/dh/configservice/ConfigServiceApplication.java
@@ -1,8 +1,15 @@
 package com.dh.configservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -12,4 +19,18 @@ public class ConfigServiceApplication {
         SpringApplication.run(ConfigServiceApplication.class, args);
     }
 
+}
+
+@RestController
+class ConfigService{
+
+    @Value("${global-message}")
+    private String globalMessage;
+
+    @RequestMapping(method = RequestMethod.GET, path="config-service")
+    public Map<String, String> message(){
+        Map<String, String> response = new HashMap<>();
+        response.put("global-message", globalMessage);
+        return response;
+    }
 }
